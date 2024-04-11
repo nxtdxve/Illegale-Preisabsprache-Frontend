d14082b87b2c5c80cc7f318bf8dcdc8e471aab62
ava <reindla@bzz.ch>
2024-04-11 23:50:20 +0200

    api request in divs on site

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 import React, { useState, useEffect } from 'react';
 
 
-const app = () => {
+const prod = () => {
   const [products, setProducts] = useState([]);
   useEffect(() =>{
     fetch('https://baumarkt-backend-1e27ff4f12df.herokuapp.com/products')
@@ -18,20 +18,36 @@ const app = () => {
     });
   }, []);
 
+  const [retailers, setRetailers] = useState([]);
+  useEffect(() =>{
+    fetch('https://baumarkt-backend-1e27ff4f12df.herokuapp.com/retailers')
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      setRetailers(data);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+  }, []);
+
 return (
+
   <div className="product-container">
     {products.map((product) => {
       return (
         <div>
           <p className="product-name">{product.name}</p>
-          <p className="product-price-hornbach">Hornbach: CHF {product.price_details/price_records}</p>
-          <p className="product-price-bauhaus">Bauhaus: CHF {product.price_details/price_records}</p>
+          {retailers.map((retailer) => {
+          <p className="product-price-hornbach">{retailer.name}: CHF {product.price_details.price_records.price}</p>
+          })}
         </div>
       );
+
     })}
   </div>
 )
 
 };
 
-export default app;
+export default prod;
